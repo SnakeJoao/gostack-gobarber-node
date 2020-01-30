96f595118a41d36b5199a4d08d52b05e5558d769
Joao Guilherme <jguipanza@gmail.com>
2020-01-30 12:59:37 -0300

    feat: Criando cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'João',
-    email: 'joao@teste.com.br',
-    password_hash: '12341412',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
